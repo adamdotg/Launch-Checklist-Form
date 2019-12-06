90018bdca9bb18cc0654d9b55c926958e81f2196
adamdotg <adamdotg@gmail.com>
2019-12-06 08:43:28 -0600

    updated if statements

--- script.js
+++ script.js
@@ -6,17 +6,23 @@ window.addEventListener("load", function(){
       let coPilot = document.querySelector("input[name=copilotName]");
       let fuelLevel = document.querySelector("input[name=fuelLevel]");
       let cargoMass = document.querySelector("input[name=cargoMass]");
-      if(Number(pilot.value)){
+      if (Number(pilot.value)) {
          alert(`${pilot.value} is invalid. Please enter a name`);
          console.log("pilot value", pilot.value);
       }
-      else if (!Number(cargoMass.value)){
-         alert(`${cargoMass.value} is not a number`);
-         console.log("isNan test", cargoMass.value);
-      } else {
-         alert(`${cargoMass.value} is definitely a number`);
+      else if (Number(coPilot.value)) {
+         alert(`${coPilot.value} is invalid. Please enter a name`);
+         console.log("copilot value", coPilot.vlue);
       }
-      if(pilot.value === "" || coPilot.value === "" || fuelLevel.value === "" || cargoMass.value === ""){
+      else if (!Number(fuelLevel.value)){
+         alert(`${fuelLevel.value} is not a number`);
+         console.log("fuel level value", fuelLevel.value);
+      }
+      else if (!Number(cargoMass.value)){
+         alert(`${cargoMass.value} is invalid. Please enter a number`);
+         console.log("cargo mass value", cargoMass.value);
+      }
+      else if (pilot.value === "" || coPilot.value === "" || fuelLevel.value === "" || cargoMass.value === ""){
          alert("All fields required");
          event.preventDefault();
       }
