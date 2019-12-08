30cdc962d4958a7e62f40c345dc58813a8bd0d4b
adamdotg <adamdotg@gmail.com>
2019-12-08 14:21:56 -0600

    fixed script for if else and event.prevent

--- script.js
+++ script.js
@@ -7,65 +7,102 @@ window.addEventListener("load", function(){
       let coPilot = document.querySelector("input[name=copilotName]");
       let fuelLevel = document.querySelector("input[name=fuelLevel]");
       let cargoMass = document.querySelector("input[name=cargoMass]");
-      let statusCheck = document.getElementById("launchStatusCheck");
+      const statusCheck = document.getElementById("launchStatusCheck");
       let faultyItems = document.querySelector("#faultyItems");
-      let pilotStatus = document.getElementById("pilotStatus");
-       
+      let launchStatus = document.getElementById("launchStatus");
+      const fuel = document.getElementById("fuelStatus");
+      //let pilotStatus = document.getElementById("pilotStatus");
+      let test = document.getElementById("test");
+      event.preventDefault();
+      
       if (pilot.value === "" || coPilot.value === "" || fuelLevel.value === "" || cargoMass.value === ""){
          alert("All fields required");
-         event.preventDefault();
-         }  
+         }
 
       if (!isNaN(pilot.value)){
          console.log(!isNaN(pilot.value));
          alert(`Pilot name "${pilot.value}" is invalid.`);
          document.querySelector("input[name=pilotName]").focus();
          return false;
-         //event.preventDefault();
       }
       else if(!isNaN(coPilot.value)){
          console.log(!isNaN(coPilot.value));
          alert(`Copilot name "${coPilot.value}" is invalid.`);
          document.querySelector("input[name=copilotName]").focus();
          return false;
-         //event.preventDefault();
       }         
       else if (isNaN(fuelLevel.value)){
          console.log(isNaN(fuelLevel.value));
          alert(`Fuel level "${fuelLevel.value}" is invalid. Please enter a number`);
          document.querySelector("input[name=fuelLevel]").focus();
          return false;
-         //event.preventDefault();
       }
       else if (isNaN(cargoMass.value)){
          console.log(isNaN(fuelLevel.value));
          alert(`Cargo mass "${cargoMass.value}" is invalid. Please enter a number`);
          document.querySelector("input[name=cargoMass]").focus;
          return false;
-         //event.preventDefault();
       }
-      function update(){
-         if (fuelLevel.value < 10000 || cargoMass.value > 10000){
-            faultyItems.style.visibility = "visible";
+      // function visible(){
+      //       statusCheck.innerHTML = `
+      //       <h2 id="launchStatus">Awaiting Information Before Launch</h2>
+      //       <div id="faultyItems" style="visibility: visible;">
+      //           <ol>
+      //               <li id="pilotStatus">${pilot.value}</li>
+      //               <li id="copilotStatus">${coPilot.value}</li>
+      //               <li id="fuelStatus">Fuel level high enough for launch</li>
+      //               <li id="cargoStatus">Cargo mass low enough for launch</li>
+      //           </ol>
+      //       </div>
+      //       `;
+      // }
+      // visible();
+      if (fuelLevel.value < 10000 && cargoMass.value > 10000){
+         statusCheck.innerHTML =`
+         <h2 id="launchStatus" style="color: red;">Shuttle not ready for launch</h2>
+         <div id="faultyItems" style="visibility: visible;">
+               <ol>
+                  <li id="pilotStatus">${pilot.value} is ready for launch</li>
+                  <li id="copilotStatus">${coPilot.value} is ready for launch</li>
+                  <li id="fuelStatus">Fuel level is too low for launch</li>   
+                  <li id="cargoStatus">Cargo mass is too high for launch</li>
+               </ol>
+         </div>
+         `;
+      }
+      else if (fuelLevel.value < 10000){
             statusCheck.innerHTML = `
-            <h2 id="launchStatus">Shuttle not ready for launch</h2>
-            <div  id="faultyItems" style="visibility: visible;">
-                <ol>
-                    <li id="pilotStatus">${pilot.value}</li>
-                    <li id="copilotStatus">${coPilot.value}</li>
-                    <li id="fuelStatus">Fuel level high enough for launch</li>
-                    <li id="cargoStatus">Cargo mass low enough for launch</li>
-                </ol>
+            <h2 id="launchStatus" style="color: red;">Shuttle not ready for launch</h2>
+            <div id="faultyItems" style="visibility: visible;">
+                  <ol>
+                     <li id="pilotStatus">${pilot.value} is ready for launch</li>
+                     <li id="copilotStatus">${coPilot.value} is ready for launch</li>
+                     <li id="fuelStatus">Fuel level is too low for launch</li>
+                     <li id="cargoStatus">Cargo mass low enough for launch</li>
+                  </ol>
             </div>
-            `
-
-            event.preventDefault();
-         }
+         `;
       }
-      update();
+      else if (cargoMass.value > 10000){
+         statusCheck.innerHTML = `
+         <h2 id="launchStatus" style="color: red;">Shuttle not ready for launch</h2>
+         <div id="faultyItems" style="visibility: visible;">
+               <ol>
+                  <li id="pilotStatus">${pilot.value} is ready for launch</li>
+                  <li id="copilotStatus">${coPilot.value} is ready for launch</li>
+                  <li id="fuelStatus">Fuel level is high enough for launch</li>   
+                  <li id="cargoStatus">Cargo mass is too high for launch</li>
+               </ol>
+         </div>
+         `;
+      } 
+      else {
+         statusCheck.innerHTML =`
+         <h2 id="launchStatus" style="color: green;">Shuttle ready for launch</h2>
+         `
+      }
 
    });
-   console.log("Loaded!");
 });
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
